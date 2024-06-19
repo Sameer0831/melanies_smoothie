227e4599994300db05fa79254a8ba72701837146
Sameer0831 <147880342+Sameer0831@users.noreply.github.com>
2024-06-19 11:34:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write(
 # st.write("You favourite fruit is:", option)
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
